0c1b56a4bc6e3d8bbcf818562822f8d70a0ed22f
uneeth-Gaikwad <puneethgaikwadl@gmail.com>
2023-06-17 23:06:47 +0530

    Solution Added

--- src/main/java/com/driver/StudentAccount.java
+++ src/main/java/com/driver/StudentAccount.java
@@ -10,4 +10,11 @@ public class StudentAccount extends BankAccount{
 
     }
 
+    public String getInstitutionName() {
+        return institutionName;
+    }
+
+    public void setInstitutionName(String institutionName) {
+        this.institutionName = institutionName;
+    }
 }
